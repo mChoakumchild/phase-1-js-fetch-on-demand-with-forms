46f9ddda13411c5e974f8cf03988cd47f594ccf7
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-12 00:13:15 -0400

    commit tree 9e0d66c1351b7413275079d98b9094e41ce5e5d7

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector('form')
 
+  inputForm.addEventListener('submit',function(e){
+    e.preventDefault();
+      let usrInput = e.target.children[1].value
+      let movieSum = document.querySelector('section#movieDetails p')
+      let movieTitle = document.querySelector('section#movieDetails h4')
+
+      fetch(`http://localhost:3000/movies/${usrInput}`)
+      .then(response => response.json())
+      .then(data => { 
+          movieTitle.innerText = data.title
+          movieSum.innterText = data.summary
+        });
+    });
+}
 document.addEventListener('DOMContentLoaded', init);
